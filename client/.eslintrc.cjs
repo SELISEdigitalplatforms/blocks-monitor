5f244228a21e5abb76e272be189e8a1510a8b868
Sk. Zaber Ahmed <zaber.ahmed@selisegroup.com>
2026-05-05 15:14:19 +0600

    refactor: made reusable back button

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -25,6 +25,8 @@ module.exports = {
     "plugin:react-hooks/recommended",
   ],
   rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": [
       "warn",
