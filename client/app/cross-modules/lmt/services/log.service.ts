59813948017bcc87914343f31fefbd7a8a177d53
Sk. Zaber Ahmed <zaber.ahmed@selisegroup.com>
2026-05-08 16:06:59 +0600

    fix: type errors

--- client/app/cross-modules/lmt/services/log.service.ts
+++ client/app/cross-modules/lmt/services/log.service.ts
@@ -1,18 +1,16 @@
 import { http } from "@/lib/http-client";
-import {
+import type {
   IGetLiveLogsPayload,
   IGetLogsByDatePayload,
   IGetLogsPayload,
   ILog,
 } from "../models/log.model";
-import { ApiPaginatedResponse } from "@/models/api-response.model";
+import type { ApiPaginatedResponse } from "@/models/api-response.model";
 import { LOG_ENDPOINTS } from "../constants/endpoint.constant";
 
 export class LogService {
-  async getLogs(
-    payload: IGetLogsPayload,
-  ): Promise<ApiPaginatedResponse<ILog[]>> {
-    return http.post<ApiPaginatedResponse<ILog[]>>(
+  async getLogs(payload: IGetLogsPayload): Promise<ApiPaginatedResponse<ILog>> {
+    return http.post<ApiPaginatedResponse<ILog>>(
       LOG_ENDPOINTS.GET_LOGS,
       payload,
     );
@@ -20,17 +18,17 @@ export class LogService {
 
   async getLogsByDate(
     payload: IGetLogsByDatePayload,
-  ): Promise<ApiPaginatedResponse<ILog[]>> {
-    return http.post<ApiPaginatedResponse<ILog[]>>(
+  ): Promise<ApiPaginatedResponse<ILog>> {
+    return http.post<ApiPaginatedResponse<ILog>>(
       LOG_ENDPOINTS.GET_LOGS_BY_DATE,
       payload,
     );
   }
 
   async getLiveLog(
-    paylaod: IGetLiveLogsPayload,
-  ): Promise<ApiPaginatedResponse<ILog[]>> {
-    const url = `${LOG_ENDPOINTS.LIVE}?Name=${paylaod.serviceName}&LastDate=${paylaod.lastDate}&ProjectKey=${paylaod.projectKey}`;
-    return http.get<ApiPaginatedResponse<ILog[]>>(url);
+    payload: IGetLiveLogsPayload,
+  ): Promise<ApiPaginatedResponse<ILog>> {
+    const url = `${LOG_ENDPOINTS.LIVE}?Name=${payload.serviceName}&LastDate=${payload.lastDate}&ProjectKey=${payload.projectKey}`;
+    return http.get<ApiPaginatedResponse<ILog>>(url);
   }
 }
