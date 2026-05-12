f5882d006f831e9b564f5d6a24f31224820248bb
Sk. Zaber Ahmed <zaber.ahmed@selisegroup.com>
2026-05-12 14:12:57 +0600

    fix: layout loading issue

--- client/app/routes/dashboard/health.tsx
+++ client/app/routes/dashboard/health.tsx
@@ -1,9 +1,5 @@
 import Health from "@blocks-devops/pages/health/health";
 
 export default function HealthPage() {
-  return (
-    <main className="p-6">
-      <Health />
-    </main>
-  );
+  return <Health />;
 }
