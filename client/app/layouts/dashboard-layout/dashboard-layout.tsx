567f31802c381ad437a2eb97d4fb932ddc989fa8
Sk. Zaber Ahmed <zaber.ahmed@selisegroup.com>
2026-05-17 20:45:08 +0600

    update: uncomment impersonate guard

--- client/app/layouts/dashboard-layout/dashboard-layout.tsx
+++ client/app/layouts/dashboard-layout/dashboard-layout.tsx
@@ -1,5 +1,5 @@
 import { DashboardLayoutProvider } from "@/contexts/dashboard-layout-provider";
-import { ProtectedGuard } from "@/guards/protected-guard";
+import { ImpersonateGuard, ProtectedGuard } from "@/guards/protected-guard";
 import { DashboardHeader } from "@/layouts/dashboard-header/dashboard-header";
 import { SidebarMenuDesktop } from "@/layouts/sidebar-menu-desktop/sidebar-menu-desktop";
 import { Outlet } from "react-router-dom";
@@ -7,19 +7,19 @@ import { Outlet } from "react-router-dom";
 export default function DashboardLayout() {
   return (
     <ProtectedGuard>
-      {/* <ImpersonateGuard> */}
-      <DashboardLayoutProvider isOpen={true} persist>
-        <div className="relative flex h-screen overflow-hidden bg-[hsl(var(--surface-app))]">
-          <SidebarMenuDesktop />
-          <div className="flex min-w-0 flex-1 flex-col overflow-hidden">
-            <DashboardHeader />
-            <main className="relative flex-1 overflow-auto p-2 sm:p-4 lg:p-6">
-              <Outlet />
-            </main>
+      <ImpersonateGuard>
+        <DashboardLayoutProvider isOpen={true} persist>
+          <div className="relative flex h-screen overflow-hidden bg-[hsl(var(--surface-app))]">
+            <SidebarMenuDesktop />
+            <div className="flex min-w-0 flex-1 flex-col overflow-hidden">
+              <DashboardHeader />
+              <main className="relative flex-1 overflow-auto p-2 sm:p-4 lg:p-6">
+                <Outlet />
+              </main>
+            </div>
           </div>
-        </div>
-      </DashboardLayoutProvider>
-      {/* </ImpersonateGuard> */}
+        </DashboardLayoutProvider>
+      </ImpersonateGuard>
     </ProtectedGuard>
   );
 }
