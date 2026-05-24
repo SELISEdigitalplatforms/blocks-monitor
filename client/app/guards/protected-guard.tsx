16c1a6b92e5099b3d8d589d0261462a0a90f9be3
asifrafeen <asif.rafeen@selisegroup.com>
2026-05-24 11:37:53 +0600

    fixes

--- client/app/guards/protected-guard.tsx
+++ client/app/guards/protected-guard.tsx
@@ -101,7 +101,7 @@ export function ImpersonationSynchronizer({
     setIsImpersonating(true);
 
     const payload: ImpersonationRequest = {
-      targetTenantId: selectedProject.tenantId,
+      targeted_tenant_id: selectedProject.tenantId,
     };
     mutateAsync(payload)
       .then(() => {
