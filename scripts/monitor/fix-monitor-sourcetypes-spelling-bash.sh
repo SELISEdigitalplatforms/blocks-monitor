4f6bb1dcc7abab4d07c2acbcd3a236521d1dcbd8
Sk. Zaber Ahmed <zaber.ahmed@selisegroup.com>
2026-05-26 20:01:01 +0600

    update: scripts

--- scripts/monitor/fix-monitor-sourcetypes-spelling-bash.sh
+++ scripts/monitor/fix-monitor-sourcetypes-spelling-bash.sh
@@ -4,7 +4,7 @@
 # CONFIG
 # =========================
 
-MONGO_URI="mongodb://10.10.64.11:27017"
+MONGO_URI="mongodb://127.0.0.1:27017"
 DATABASE_NAME="yourDatabaseName"       # 👈 set your database name
 
 OLD_FIELD="MonitorSourcetypes"
