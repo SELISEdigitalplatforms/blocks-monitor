004133367342077a22945542282b26151c947fcd
Sk. Zaber Ahmed <zaber.ahmed@selisegroup.com>
2026-06-08 15:18:12 +0600

    fix: build

--- client/app/constants/breadcrumb-custom-title.constant.ts
+++ client/app/constants/breadcrumb-custom-title.constant.ts
@@ -4,6 +4,7 @@ export const BREADCRUMB_CUSTOM_TITLES: Record<RouterType, string | null> = {
   "/": null,
   "/console": null,
   "/dashboard": null,
+  "/project-overview": null,
   "/project-overview/environments": null,
   "/health": null,
   "/health/monitor/incidents/:id": null,
