ef56254d8424d6c75ee8337e21becba8601293f2
Sk. Zaber Ahmed <zaber.ahmed@selisegroup.com>
2026-06-22 12:50:44 +0600

    update: api docs button

--- client/app/layouts/health-layout/health-layout.tsx
+++ client/app/layouts/health-layout/health-layout.tsx
@@ -1,9 +1,25 @@
+import { Button } from "@/components/ui-kits/button/button";
+import { getRuntimeEnv } from "@seliseblocks/blocks-kit";
 import { Outlet } from "react-router-dom";
 
 export function HealthLayout() {
   return (
-    <main className="p-6">
+    <main className="p-6 space-y-6">
+      <HealthHeader />
       <Outlet />
     </main>
   );
 }
+
+const HealthHeader = () => {
+  return (
+    <div className="flex items-center justify-end">
+      <a
+        href={getRuntimeEnv("BLOCKS_MONITOR_BASE_URL") + "/swagger/index.html"}
+        target="_blank"
+        rel="noopener noreferrer">
+        <Button variant="outline">API Docs</Button>
+      </a>
+    </div>
+  );
+};
